482b5d80d27724d5a8e0f731d26597a450ef281b
Victor <victormeyer2012@hotmail.com>
2025-10-10 20:56:49 -0300

    components and previews

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,56 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+@Preview
+@Composable
+private fun BookSearchBarPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    )
+    BookSearchBar(
+        searchQuery = "",
+        onSearchQueryChange = {},
+        onImeSearch = {},
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Title $it",
+        authors = listOf("Author $it"),
+        imageUrl = "https://test.com",
+        description = "Dio $it",
+        languages = emptyList(),
+        averageRating = 4.67835,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 4,
+        firstPublishYear = null
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
+}
